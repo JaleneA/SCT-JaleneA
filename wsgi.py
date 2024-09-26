d703a4efc882e46f5fe35fbe78d6ff6035229844
Jalene Is'raell Armstrong <123189357+JaleneA@users.noreply.github.com>
2024-09-26 09:49:14 -0400

    Deleted unneeded files

--- wsgi.py
+++ wsgi.py
@@ -7,9 +7,6 @@ from App.models import User
 from App.main import create_app
 from App.controllers import ( create_user, get_all_users_json, get_all_users, initialize )
 
-
-# This commands file allow you to create convenient CLI commands for testing controllers
-
 app = create_app()
 migrate = get_migrate(app)
 
@@ -30,16 +27,18 @@ User Commands
 user_cli = AppGroup('user', help='User object commands') 
 
 # Then define the command and any parameters and annotate it with the group (@)
-@user_cli.command("create", help="Creates a user")
-@click.argument("username", default="rob")
+@user_cli.command("create", help="Creates a staff account")
+@click.argument("email", default="rob@mail.com")
+@click.argument("firstname", default="rob")
+@click.argument("lastname", default="bob")
 @click.argument("password", default="robpass")
-def create_user_command(username, password):
-    create_user(username, password)
-    print(f'{username} created!')
+def create_user_command(email, firstname, lastname, password):
+    create_user(email, firstname, lastname, password)
+    print(f'{firstname + " " + lastname} created!')
 
 # this command will be : flask user create bob bobpass
 
-@user_cli.command("list", help="Lists users in the database")
+@user_cli.command("list", help="Lists all staff members in the database")
 @click.argument("format", default="string")
 def list_user_command(format):
     if format == 'string':
@@ -48,22 +47,3 @@ def list_user_command(format):
         print(get_all_users_json())
 
 app.cli.add_command(user_cli) # add the group to the cli
-
-'''
-Test Commands
-'''
-
-test = AppGroup('test', help='Testing commands') 
-
-@test.command("user", help="Run User tests")
-@click.argument("type", default="all")
-def user_tests_command(type):
-    if type == "unit":
-        sys.exit(pytest.main(["-k", "UserUnitTests"]))
-    elif type == "int":
-        sys.exit(pytest.main(["-k", "UserIntegrationTests"]))
-    else:
-        sys.exit(pytest.main(["-k", "App"]))
-    
-
-app.cli.add_command(test)
